315544a99daea8b85698a229a960b909ab6375cd
Vahidone <v.ban.roma@gmail.com>
2023-11-23 18:32:14 +0100

    feat: create header with menu

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,6 +11,7 @@
 
 </head>
 <body>
+    @include('partials.header')
 
     <main class="container my-5 py-5">
 
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,20 @@
+
+
+<header>
+    <ul class="nav">
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Comics list</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Details</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">About us</a>
+        </li>
+    </ul>
+
+</header>
