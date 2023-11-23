2eb88dbd7c08da2872e1961a63de3cc0ca27a505
Vahidone <v.ban.roma@gmail.com>
2023-11-23 22:59:15 +0100

    feat: make details page

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -7,28 +7,27 @@
 
 <h1 class="text-center text-success fw-bold fs-1 mb-5">Comics Details</h1>
 
-<div class="container">
+<div class="container text-center">
 
-    @foreach ($comics as $comic)
+    <div class="card-wraper w-100 d-flex justify-content-center">
 
-        <div class="card m-3" style="width: 18rem;">
+
+        <div class="card m-3 w-50" style="width: 18rem;">
             <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}">
             <div class="card-body">
-            <h5 class="card-title">{{ $comic->title }}</h5>
-            <p class="card-title">{{ $comic->description }}</p>
-            <p class="card-title">{{ $comic->sale_date }}</p>
-            <p class="card-text">{{ $comic->artisti }}</p>
-            <p class="card-text">{{ $comic->price }}</p>
-            <p class="card-text">{{ $comic->type }}</p>
-            <p class="card-text">{{ $comic->type }}</p>
+                <h5 class="card-title">{{ $comic->title }}</h5>
+                <p class="card-title">{{ $comic->description }}</p>
+                <p class="card-title">{{ $comic->sale_date }}</p>
+                <p class="card-text">{{ $comic->artisti }}</p>
+                <p class="card-text">{{ $comic->price }}</p>
+                <p class="card-text">{{ $comic->type }}</p>
+                <p class="card-text">{{ $comic->type }}</p>
 
-            <a href="#" class="btn btn-primary">Details</a>
             </div>
         </div>
 
 
-
-    @endforeach
+    </div>
 
     <a href="{{ route('home')}}" class="btn btn-danger mt-5">Torna in homepage</a>
 
