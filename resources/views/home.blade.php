9d76283bfe47d60897ec44cad6888a2a0e8e6293
Vahidone <v.ban.roma@gmail.com>
2023-11-25 16:37:11 +0100

    text: change subtitle in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 
     <h1 id="home-title">MONDO FUMETTI</h1>
 
-    <p class="text-primary fs-3 ">Sono presenti <span class="text-danger fs-2 fw-bold">{{$num_products}}</span> fumetti nel nostro negozio online</p>
+    <p class="text-primary fs-3 ">Attualmente abbiamo <span class="text-danger fs-2 fw-bold">{{$num_products}}</span> fumetti nel nostro negozio online</p>
 
     <p class="text-success fs-4 fw-bold mt-5">Dai un'occhiata alla lista dei fumetti disponibili :D</p>
 </div>
